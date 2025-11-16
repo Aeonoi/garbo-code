535a4706ec094afa44587e496ea729f0eeb9fb91
Aeonoi <dzhou527@gmail.com>
2025-11-16 00:18:10 -0500

    update stuff

--- garbo.sh
+++ garbo.sh
@@ -2,13 +2,14 @@
 
 # This script is intentionally ugly and poorly written.
 # It shuffles a list of languages and executes a corresponding binary.
+# Modified so that each script's output feeds into the next one.
 
 # Define an array of languages
 langs=("cpp" "py" "js" "go" "rs" "zig" "java" "hs" "pl" "scm" "ml" "c" "ts" "perl" "vim" "lua")
 
-# Shuffle the array (ugliest way possible)
+# Shuffle the array (still ugly)
 for (( i=${#langs[@]}-1; i>0; i-- )); do
-    j=$(( $RANDOM % (i+1) ))
+    j=$(( RANDOM % (i+1) ))
     temp=${langs[i]}
     langs[i]=${langs[j]}
     langs[j]=$temp
@@ -21,13 +22,21 @@ else
     inpt="$1"
 fi
 
-# Loop through shuffled languages and execute their scripts
+# Loop through shuffled languages
 for lang in "${langs[@]}"; do
-    cat "$lang".sh
-    echo "$inpt" | ./"$lang".sh
+    echo "=== Running $lang.sh ==="
+    cat "$lang.sh"
+    echo "Input: $inpt"
+
+    # Capture output and store it in inpt
+    inpt="$(echo "$inpt" | "./$lang.sh")"
+
+    echo "Output -> New input: $inpt"
+    echo
 done
 
-cd ./HackRPI-2025/
+# Final steps
+cd ./HackRPI-2025/ || exit 1
 make
+./asteroids "$inpt"
 
-./asteroids  $inpt
