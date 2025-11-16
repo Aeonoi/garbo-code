16aff8468d3f71d1f9094258f9762f033dac17d8
Aeonoi <dzhou527@gmail.com>
2025-11-15 22:22:54 -0500

    add main file

---
+++ defines.h
@@ -0,0 +1,23 @@
+#define a int
+#define aa main
+#define aaa return
+#define aaaa 0
+#define aaaaa {
+#define aaaaaa }
+#include <iostream>
+#define AA int
+#define AAA main
+#define AAAAAAAA {
+#define AAAAAAAAAA }
+#define AAAAAAAAAAA return
+#define AAAAAAAAAAAA for
+#define AAAAAAAAAAAAA while
+#define AAAAAAAAAAAAAAA if
+#define AAAAAAAAAAAAAAAAA std
+#define AAAAAAAAAAAAAAAAAAA cout
+#define AAAAAAAAAAAAAAAAAAAA ==
+#define AAAAAAAAAAAAAAAAAAAAA "Hello, World!\r\n"
+#define AAAAAAAAAAAAAAAAAAAAAA ::
+#define AAAAAAAAAAAAAAAAAAAAAAAAA =
+#define AAAAAAAAAAAAAAAAAAAAAAAAAAA <<
+#define AAAAAAAAAAAAAAAAAAAAAAAAAAAA I
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "defines.h"
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
